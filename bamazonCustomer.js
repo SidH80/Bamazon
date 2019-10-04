c48874fdea1c5ba0fe16e54a154a1c443b2bae47
lethalSid <senien.hicks@gmail.com>
2019-10-03 23:38:06 -0400

    pulled products from database and added inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 //Ask WHat would you like to do?
 //Display Items in a nice formed table
-//Propmt User for ID item off the table
-//Promt please enter a quanitity that you'd like to purchase
+//Prompt User for ID item off the table
+//Prompt please enter a quantity that you'd like to purchase
     //Make a decision - do we have enough?
         //if yes log great
             //show total cost and update database
@@ -10,11 +10,14 @@
             //re-prompt
 
 //initialize dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 
 // initialize connection variable for mysql database
 
 var connection = mysql.createConnection ({
-    host: "localhost",,
+    host: "localhost",
     port: 3306,
     user:"root",
     password: "root",
@@ -22,34 +25,77 @@ var connection = mysql.createConnection ({
 })
 
 //create connection with the server and load the product data
+
 connection.connect(function(err) {
-    if (err) {
-        console.log(`error connect ${err}`);
-    }
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
     loadProducts();
-})
+  });
 
 function loadProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
     if(err) throw err;
 
-
     //displays items
     console.table(res);
     //the prompt the customer for an item
-    prompCustomerForItem();
+    promptCustomerForItem();
+    connection.end();
     })
 }
 
-function prompCustomerForItem() {
+function promptCustomerForItem() {
+    //Ask the customer the ID of the product they would like to buy.
+    inquirer
+        .prompt([
+            {
+            name: "item_id",
+            type: "input",
+            message: "Please enter the ID of the item you'd like to purchase",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+                }
+            }
+        ])
+        .then(function(answer) {
+            console.log(answer.item_id);
+            var query = `SELECT id FROM products WHERE ?`
+            connection.query(query, answer.item_id, function(err, res){
+                console.table(res);
+            })
+            promptQuantity();
+        });
 
 }
 
-function prompCustomerForQuantity() {
+
+function promptQuantity (item_id){
+    inquirer
+        .prompt([
+        {
+            name: "item_quantity",
+            type: "input",
+            message: "How many items would you like to purchase?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                return true;
+                }
+                return false;
+                }
+        }
+        ])
+        .then(function(answer) {
+            console.log(answer.item_quantity)
+            checkInventory();
+        });
+
 
 }
 
-function makePurchase {
+function makePurchase() {
 
 }
 
@@ -57,6 +103,11 @@ function makePurchase {
     //use a for loop and a comparison
 function checkInventory() {
 
+    //if we have enough items to sell {
+        //multiply item_quantity by price and log amount of purchase
+    //} else {
+        //re-prompt how many more items how many would you like to purchase
+    //}
 }
 
 //check to see if the user wants to exit
--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -1,32 +1,34 @@
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
+USE bamazon_db;
+
 CREATE TABLE products (
-    id INT AND_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     product VARCHAR (255) NOT NULL,
     department VARCHAR (255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    stock_quanitity INT (10) NOT NULL,
+    price DECIMAL(10,3) NOT NULL,
+    quantity INT(10) NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Porsche", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
-INSERT INTO products (product, department, price, stock_quanitity)
-VALUES ("Corvette", 'Automobile', 50000.99, 5),
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Porsche", "Automobile", "95000", 25);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Corvette", "Automobile", "65000", 25);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Toothbrush", "Oral", "2.50", 105);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Pen", "Suplies", "1.99", 100);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("TV", "Electronics", "850.99", 90);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Sofa", "Furniture", "899.99", 5);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Table", "Furniture", "450.99", 65);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Laptop", "Computers", "1200", 2);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Headphones", "Accessories", "49.99", 25);
+INSERT INTO products (product, department, price, quantity)
+VALUES ("Samsung S9", "Cell", "1200", 5);
