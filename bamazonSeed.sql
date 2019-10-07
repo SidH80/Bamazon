0e725c3d74e005e44e585ce73cfffc39a73998c9
lethalSid <senien.hicks@gmail.com>
2019-10-06 22:15:24 -0400

    updated bamseed and added table dept to bzon databse

--- bamazonSeed.sql
+++ bamazonSeed.sql
@@ -3,16 +3,24 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE products (
+CREATE TABLE inventory (
     id INT AUTO_INCREMENT NOT NULL,
     product VARCHAR (255) NOT NULL,
     department VARCHAR (255) NOT NULL,
-    price DECIMAL(10,3) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     quantity INT(10) NOT NULL,
+    product_sales INT(10) default 0,
     PRIMARY KEY (id)
 );
 
-use bamazon_db;
+CREATE TABLE departments (
+    dept_id INT AUTO_INCREMENT NOT NULL,
+    dept_name VARCHAR (255) NOT NULL,
+    over_head_costs DECIMAL(10,3) NOT NULL,
+    product_sales INT(10) NOT NULL,
+    total_profit DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (dept_id)
+);
 
 insert into inventory (product, department, price, quantity)
 values("widget", "Supplies", .99, 100);
@@ -46,3 +54,9 @@ values("Jeans", "Clothing", 120.99, 11);
 
 insert into inventory (product, department, price, quantity)
 values("Pumps", "Shoes", 199.99, 22);
+
+insert into departments (department_name, over_head_costs, price, total_profit)
+values("Electronics", 10000, 20000, 10000);
+
+insert into departments (department_name, over_head_costs, price, total_profit)
+values("Automobiles", 200000, 300000, 150000);
